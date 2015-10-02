8c56c8473fa9ef2ab4fcd39e214a78cd37166888
cgmuros <cgmuros@gmail.com>
2015-10-02 14:01:36 -0300

    Probando

---
+++ app/controllers/agregar.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+	router = express.Router(),
+	mongoose = require('mongoose'),
+	Camisetas = mongoose.model('camisetas');
+
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/agregar', function(peticion, respuesta) {
+	
+	respuesta.render('agregar');
+	
+});
+
+router.post('/agregar', function(peticion, respuesta) {
+	
+	var nuevoRegistro = new Camisetas({
+		color: peticion.body.color,
+		precio: peticion.body.precio,
+		descripcion: peticion.body.descripcion,
+		imagen: peticion.body.imagen
+	});
+	
+	nuevoRegistro.save();
+	
+	respuesta.redirect('/tienda');
+	
+} )
